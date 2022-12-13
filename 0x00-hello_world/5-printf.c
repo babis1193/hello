075a725b36d76cc481428fc4ca57ac6b973ff71a
babis1193 <babalolaismael@gmail.com>
2022-12-12 20:08:21 -0800

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- * main - print a c program using printf
- *
+ * main - print a c program using printf*
  *
  * Return: 0
  *
@@ -12,12 +10,9 @@
 
 
 int main(void)
-
 {
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
 
