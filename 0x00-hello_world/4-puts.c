0a94904ae5cb1e638baa1441d3386a47885b7229
babis1193 <babalolaismael@gmail.com>
2022-12-12 18:53:28 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * * main - print a line of code using puts
+ *
+ * *
+ *
+ * * Description: A C program that prints with exactly
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	        puts("\"Programming is like building a multilingual puzzle");
+
+		        return (0);
+
+}
